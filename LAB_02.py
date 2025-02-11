3a46d83daba4ab923847f0cde614d1a6841024c8
Joseph-Rodriguez21 <est.joseph.rodriguez@unimilitar.edu.co>
2025-02-10 20:17:42 -0500

    Add files via upload

---
+++ LAB_02.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#DATOS JOSEPH RODRIGUEZ
+j = np.array([5,6,0,0,8,3,5])
+r = np.array([1,0,7,6,2,4,2,3,6,2])
+
+y1 = np.convolve(j, r)
+y_nj = np.arange(len(y1))
+print("JOSEPH y[n] =", y1)
+
+plt.stem(y_nj, y1)
+plt.xlabel("n")
+plt.ylabel("y[n]")
+plt.title("Convolución de j[n] y r[n] (JOSEPH RODRIGUEZ)")
+plt.grid()
+plt.show()
+
+#DATOS FELIPE TORRES 
+f = np.array([5,6,0,0,8,2,5])
+t = np.array([1,0,1,2,3,4,1,6,6,7])
+
+y2 = np.convolve(f, t)
+y_nf = np.arange(len(y2))
+print("FELIPE y[n] =", y2)
+
+plt.stem(y_nf, y2)
+plt.xlabel("n")
+plt.ylabel("y[n]")
+plt.title("Convolución de f[n] y t[n] (FELIPE TORRES)")
+plt.grid()
+plt.show()
+
+#DATOS EEEE YEEEIDER ORTIZ
+y = np.array([5,6,0,0,7,8,2])
+o = np.array([1,0,6,9,2,8,2,0,4,8])
+
+y3 = np.convolve(y, o)
+y_ny = np.arange(len(y3))
+print("YADER  y[n] =", y3)
+
+plt.stem(y_ny, y3)
+plt.xlabel("n")
+plt.ylabel("y[n]")
+plt.title("Convolución de y[n] y o[n] (YADER ORTIZ)")
+plt.grid()
+plt.show()
+
+
+#SEGUNDA PARTE LABORATORIO 
+
+
+# Parámetros
+fs = 1000  # Frecuencia de muestreo (Hz)
+Ts = 1.25e-3  # Período de muestreo (s)
+n = np.arange(9)  # Vector de tiempo discreto
+
+# Señales
+x1 = np.cos(2 * np.pi * 100 * n * Ts)
+x2 = np.sin(2 * np.pi * 100 * n * Ts)
+
+# Cálculo de la correlación
+correlation = np.correlate(x1, x2, mode='full')
+lag = np.arange(-len(x1) + 1, len(x1))
+
+# Crear figura con tres subgráficos
+plt.figure(figsize=(10, 7))
+
+# Gráfica de x1[n]
+plt.subplot(3, 1, 1)
+plt.stem(n, x1, linefmt='b-', markerfmt='bo', basefmt='k-')
+plt.xlabel('n')
+plt.ylabel('Amplitud')
+plt.title('Señal x1[n] = cos(2π100nTs)')
+plt.grid()
+
+# Gráfica de x2[n]
+plt.subplot(3, 1, 2)
+plt.stem(n, x2, linefmt='g-', markerfmt='go', basefmt='k-')
+plt.xlabel('n')
+plt.ylabel('Amplitud')
+plt.title('Señal x2[n] = sin(2π100nTs)')
+plt.grid()
+
+# Gráfica de la correlación
+plt.subplot(3, 1, 3)
+plt.stem(lag, correlation, linefmt='r-', markerfmt='ro', basefmt='k-')
+plt.xlabel('Desplazamiento')
+plt.ylabel('Correlación')
+plt.title('Correlación entre x1[n] y x2[n]')
+plt.grid()
+
+# Mostrar la figura
+plt.tight_layout()
+plt.show()
